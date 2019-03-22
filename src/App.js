18a75ac9ebc94495977c872cf0b811b75ec13a40
ali1129js <ali1129js@gmail.com>
2019-03-22 09:55:35 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/**
+ * @Author: Ali
+ * @Date:   2019-03-22T09:54:56+01:00
+ * @Last modified by:   Ali
+ * @Last modified time: 2019-03-22T09:54:56+01:00
+ */
+
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="App-header">WordBeater</header>
       </div>
     );
   }
