433a090c8ffe72583428945e96c344a28f1dfd30
ali1129js <ali1129js@gmail.com>
2019-03-24 19:09:23 +0100

    Updates

---
+++ precache-manifest.b0d1737e9b97603064f55534aba06820.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "58a16a8c1a5c75f20b83",
+    "url": "/speedtyping-app/static/js/runtime~main.cd042114.js"
+  },
+  {
+    "revision": "12bd8ad24da8cc9efc89",
+    "url": "/speedtyping-app/static/js/main.ae092c67.chunk.js"
+  },
+  {
+    "revision": "c7c3ca8208068ab52c3d",
+    "url": "/speedtyping-app/static/js/2.8decb4fc.chunk.js"
+  },
+  {
+    "revision": "12bd8ad24da8cc9efc89",
+    "url": "/speedtyping-app/static/css/main.aa95d2ea.chunk.css"
+  },
+  {
+    "revision": "c7c3ca8208068ab52c3d",
+    "url": "/speedtyping-app/static/css/2.2097710e.chunk.css"
+  },
+  {
+    "revision": "d3fbec558731a3beccb3464cf6a87093",
+    "url": "/speedtyping-app/index.html"
+  }
+];
